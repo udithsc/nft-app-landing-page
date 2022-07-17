8eae20afd0e172c7adad81d739457b4bd3695c95
Udith Chandrarathna <udithsc@gmail.com>
2022-07-17 16:23:18 +0530

    resolved build issue warning

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,14 @@
-import homeHero from "./home_hero.png";
-import homeCards from "./home_cards.png";
-import feature from "./feature.png";
-import expo from "./expo.png";
-import expo02 from "./expo02.png";
-import javascript from "./javascript.png";
-import react from "./react.png";
-import scene from "./scene.png";
-import mockup from "./mockup.png";
+import homeHero from './home_hero.png';
+import homeCards from './home_cards.png';
+import feature from './feature.png';
+import expo from './expo.png';
+import expo02 from './expo02.png';
+import javascript from './javascript.png';
+import react from './react.png';
+import scene from './scene.png';
+import mockup from './mockup.png';
 
-export default {
+const assets = {
   homeHero,
   homeCards,
   feature,
@@ -19,3 +19,5 @@ export default {
   scene,
   mockup,
 };
+
+export default assets;
